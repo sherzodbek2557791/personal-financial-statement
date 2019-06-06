9abd2cf6f7be82965c7eca1b7c09649c07d91397
Sherzodbek <leonbek2557791@gmail.com>
2019-06-06 17:06:06 +0500

    optimize

--- financial-statement-ui/src/routes.js
+++ financial-statement-ui/src/routes.js
@@ -2,6 +2,6 @@ import Index from "./components/Index.vue";
 import Calculator from "./components/Calculator.vue";
 
 export default [
-  { path: "/", component: Index },
-  { path: "/calculator", component: Calculator }
+  { path: "/", component: Calculator },
+  { path: "/calculator", component: Index }
 ];
